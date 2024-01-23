e076cf4f01c5c2223e86038324f77ae2f2089978
munergenis <154993411+munergenis@users.noreply.github.com>
2024-01-23 19:09:52 +0100

    feat(business): add DataReader class props methods

--- Business/DataAccess.cs
+++ Business/DataAccess.cs
@@ -3,10 +3,66 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
 
 namespace Business
 {
     public class DataAccess
     {
+        private SqlConnection connection;
+        private SqlCommand command;
+        private SqlDataReader reader;
+
+        public SqlDataReader Reader { get { return reader; } }
+
+        public DataAccess()
+        {
+            connection = new SqlConnection("server=.\\SQLEXPRESS; database=CATALOGO_DB; integrated security:true");
+            command = new SqlCommand();
+        }
+
+        public void SetQuery(string query)
+        {
+            command.CommandType = CommandType.Text;
+            command.CommandText = query;
+        }
+
+        public void ExecuteQuery()
+        {
+            command.Connection = connection;
+            try
+            {
+                connection.Open();
+                reader = command.ExecuteReader();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public void ExecuteNonQuery()
+        {
+            command.Connection = connection;
+            try
+            {
+                connection.Open();
+                command.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public void CloseConnection()
+        {
+            if (reader != null)
+            {
+                reader.Close();
+            }
+            connection.Close();
+        }
     }
 }
